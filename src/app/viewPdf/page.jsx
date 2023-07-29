0afeffbb0472d8295349b5ae2e1bc4a13e5fe964
personalDenis <denis_jr2001@hotmail.com>
2023-07-29 19:24:17 -0300

    pdf started

--- src/app/viewPdf/page.jsx
+++ src/app/viewPdf/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import DecDependEconPdf from "../formDestProcCur/decFeitProcCur/pdf/DecDependEconPdf";
+
+import dynamic from "next/dynamic";
+
+const PDFViewer = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
+  { ssr: false }
+);
 
 const ViewPdf = () => {
-    return ( <>
-        view pdf
-        </> );
-}
- 
+  return (
+    <PDFViewer style={{ width: "100vw", height: "100vh" }}>
+      <DecDependEconPdf />
+    </PDFViewer>
+  );
+};
+
 export default ViewPdf;
